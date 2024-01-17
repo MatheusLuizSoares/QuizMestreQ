02c23640bf8a076868329a163d9dd218ae85bab4
Matheus Luiz Soares <matheusluiz15a@gmail.com>
2024-01-17 18:49:36 -0300

    ajeitando o forms

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 def cadastro(request):
-  return render(request,"cadastro.html")
+  if request.method=="GET":
+    return render(request,"cadastro.html")
+  elif request.method=="POST":
+    return 
