477e4336db177032f1a13cb449faa5d7aa8af5e6
Matheus Luiz Soares <matheusluiz15a@gmail.com>
2024-01-19 14:29:15 -0300

    fiz sistema de login e logaut

--- usuarios/views.py
+++ usuarios/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.messages import constants
 from django.contrib import messages
+from django.contrib import auth
 def cadastro(request):
   if request.method=="GET":
     return render(request,"cadastro.html")
@@ -27,5 +28,24 @@ def cadastro(request):
   except:
     messages.add_message(request, constants.ERROR, 'ERRO interno do servidor')
     return redirect('/usuarios/cadastro')
-
   
+def logar(request):
+   if request.method=='GET':
+     return render(request,'login.html')
+   elif request.method=="POST":
+     username= request.POST.get("username")
+     senha=request.POST.get('senha')
+     
+     user=auth.authenticate(request,username=username,password=senha)
+     
+     if user:
+       auth.login(request,user)
+       messages.add_message(request,constants.SUCCESS,'Logado!')
+       return redirect("/flashcard/novo_flashcard/")
+     else:
+       messages.add_message(request, constants.ERROR,"usurname ou senha invalidos")
+       return redirect('/usuarios/logar/')
+  
+def logout(request):
+  auth.logout(request)
+  return redirect('/usuarios/logar')
