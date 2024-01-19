5f12a8c05057ea0c58a4e5a3788e88e784f71b83
Matheus Luiz Soares <matheusluiz15a@gmail.com>
2024-01-19 17:40:29 -0300

    criando o flashcard e modificando o models para flashcard

--- flashcard/models.py
+++ flashcard/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
-# Create your models here.
+from django.contrib.auth.models import User
+class Categoria(models.Model):
+  nome= models.CharField( max_length=20)
+  
+  def __str__(self):
+    return self.nome
+class Flashcard(models.Model):
+  DIFICULDADE_CHOICE=(('D', 'Dificil')('M','Médio')("F")("Facil"))
+  user= models.ForeignKey(User, on_delete=models.DO_NOTHING)
+  pergunta=models.CharField(max_length=100)
+  resposta=models.TextField()
+  Categoria=models.ForeignKey(Categoria,on_delete=models.DO_NOTHING)
+  dificuldade=models.CharField(max_length=1,choices=DIFICULDADE_CHOICE)
+  def __str__(self):
+    return self.pergunta
