73a7ddc920c5a403a259fe687ac3e28a8736dd0a
Matheus Luiz Soares <matheusluiz15a@gmail.com>
2024-01-20 19:28:50 -0300

    fazendo reformulação no admin

--- flashcard/models.py
+++ flashcard/models.py
@@ -6,11 +6,12 @@ class Categoria(models.Model):
   def __str__(self):
     return self.nome
 class Flashcard(models.Model):
-  DIFICULDADE_CHOICE=(('D', 'Dificil')('M','Médio')("F")("Facil"))
+  DIFICULDADE_CHOICE=(('D', 'Dificil'),('M','Médio'),('F','Facil'))
   user= models.ForeignKey(User, on_delete=models.DO_NOTHING)
   pergunta=models.CharField(max_length=100)
   resposta=models.TextField()
   Categoria=models.ForeignKey(Categoria,on_delete=models.DO_NOTHING)
   dificuldade=models.CharField(max_length=1,choices=DIFICULDADE_CHOICE)
+  
   def __str__(self):
     return self.pergunta
