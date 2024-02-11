345c6d55980165bb216df908776502fc0c403990
Matheus Luiz Soares <matheusluiz15a@gmail.com>
2024-02-11 20:27:43 -0300

    verificando erros na parte do cliente

--- flashcard/views.py
+++ flashcard/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render,redirect
 from .models import Categoria,Flashcard
+from django.http import response
+from django.contrib.messages import constants
+from django.contrib import messages
 def novo_flashcard(request):
   if not request.user.is_authenticated:
     return redirect('/usuarios/logar')
@@ -9,3 +12,13 @@ def novo_flashcard(request):
     
     dificuldades=Flashcard.DIFICULDADE_CHOICE
     return render (request, "novo_flashcard.html",{'categorias':categorias,"dificuldades":dificuldades})
+  elif request.method=="POST":
+       pergunta=request.POST.get('pergunta')
+       resposta=request.POST.get('resposta')
+       categoria=request.POST.get('categoria')
+       dificuldade=request.POST.get("dificuldade")
+       if len(pergunta.strip())==0 or len(resposta.strip())==0:
+         messages.add_message(request,constants.ERROR,"Preenchar os campos de pergunta e resposta")
+         redirect("/flashcard/novo_flashcard/")
+         
+        
